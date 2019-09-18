863368fd11da71b1b502ff96982bce3161075dfd
gitmhjao <slightmirror@hotmail.com>
2019-09-19 05:38:21 +0800

    D10 upload

---
+++ D10/app_core.py
@@ -0,0 +1,29 @@
+﻿from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+line_bot_api = LineBotApi('聊天機器人的 Chennel access token')
+handler = WebhookHandler('聊天機器人的 Channel secret')
+
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+if __name__ == "__main__":
+    app.run()
---
+++ D10/app_echo.py
@@ -0,0 +1,42 @@
+﻿from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+line_bot_api = LineBotApi('聊天機器人的 Chennel access token')
+handler = WebhookHandler('聊天機器人的 Channel secret')
+
+
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# 學你說話
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+if __name__ == "__main__":
+    app.run()
